4b715352bb6ff353e3c5b3f30c020b57acadde9e
Gurukiran Badiger <96974118+gurukiranbadiger08@users.noreply.github.com>
2022-04-11 23:02:22 +0530

    Delete storage directory

--- storage/framework/views/590bb8ce3720810458fa7298bba93ce78c05002b.php
+++
@@ -1,2 +0,0 @@
-[<?php echo e($slot); ?>](<?php echo e($url); ?>)
-<?php /**PATH /home/gurukiran/Desktop/forget and reset/Secure/vendor/laravel/framework/src/Illuminate/Mail/resources/views/text/header.blade.php ENDPATH**/ ?>
