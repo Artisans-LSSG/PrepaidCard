b373a3b26c9408ecd84e1fd74281a60163102f37
Siva Malleswara Reddy Lomada <93829203+SivaMalleswaraReddy@users.noreply.github.com>
2022-04-12 16:34:24 +0530

    Add files via upload

--- database/seeders/RefundSeeder.php
+++ database/seeders/RefundSeeder.php
@@ -5,6 +5,7 @@ use App\Models\refunds;
 use App\Models\Transaction;
 use Faker\Factory;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RefundSeeder extends Seeder
 {
@@ -21,6 +22,7 @@ class RefundSeeder extends Seeder
         $trans_amount = $transaction->transaction_amount;
         $trans_status = $transaction->transaction_status;
         $transaction_date = $transaction->transaction_date;
+
         if ($trans_status == false) {
             DB::table('refunds')->insert([
                 'transaction_id' => $trans_id,
@@ -28,8 +30,18 @@ class RefundSeeder extends Seeder
                 'refund_date' => $faker->dateTimeBetween($transaction_date, '+1 week'),
                 'refund_status' => 1,
 
+
+//
+//        $faker = Factory::create();
+//        $transaction = Transaction::all()->random();
+//        $refund  = new refunds();
+//        $refund -> refund_amount = $faker->numberBetween(100,5000);
+//        $transaction->transaction_amount = $faker->numberBetween(1000,5000);
+//        $refund->refund_amount=$transaction->transaction_amount;
+//        $refund->refund_date = $transaction->transaction_date;
+//        $transaction->transaction_id = $transaction->id;
             ]);
         }
-
     }
+
 }
