20041c81365b118678d10b66a92d15095d31b548
Siva Malleswara Reddy Lomada <93829203+SivaMalleswaraReddy@users.noreply.github.com>
2022-05-18 09:41:08 +0530

    Add files via upload

--- routes/api.php
+++ routes/api.php
@@ -26,9 +26,7 @@ Route::resource('/cards',\App\Http\Controllers\CardController::class);
 Route::resource('/transaction',\App\Http\Controllers\TransactionController::class);
 Route::resource('/vendor',\App\Http\Controllers\VendorController::class);
 Route::resource('/refunds',\App\Http\Controllers\RefundController::class);
-//Route::resource('/admin',\App\Http\Controllers\AdminController::class);
 
-//Route::post("login",[UserController::class,'index']);
 
 Route::post('/parent/child',[\App\Http\Controllers\ParentUserController::class,'storechild']);
 Route::post('/parent/save',[\App\Http\Controllers\ParentUserController::class,'store']);
@@ -44,9 +42,7 @@ Route::post('/admin/childreject',[\App\Http\Controllers\AdminController::class,'
 
 
 Route::get('/admin/childrequeststatus',[\App\Http\Controllers\AdminController::class,'childrequestStatus']);
-
 Route::get('/admin/request-status',[\App\Http\Controllers\AdminController::class,'requestStatus']);
-
 Route::get('/parents/transaction/child/{first_name}',[\App\Http\Controllers\ParentUserController::class,'showtransaction']);
 Route::get('/parents/child/{first_name}',[\App\Http\Controllers\ParentUserController::class,'showchild']);
 Route::get('transaction/card/{card_number}',[\App\Http\Controllers\TransactionController::class,'showcard']);
@@ -58,7 +54,6 @@ Route::get('/admin/refund/{id}',[\App\Http\Controllers\AdminController::class,'s
 Route::get('/admin/card/{card_number}',[\App\Http\Controllers\AdminController::class,'showcard']);
 Route::get('/admin/child/{id}',[\App\Http\Controllers\AdminController::class,'showchild']);
 Route::get('/admin/parent/{id}',[\App\Http\Controllers\AdminController::class,'showparent']);
-
 Route::get('/showchild/{card_number}',[\App\Http\Controllers\CardController::class,'showchild']);
 
 
